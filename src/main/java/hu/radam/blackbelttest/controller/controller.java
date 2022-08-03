08cf91d9fd6ce07b99e8569b7a74a0b4d4af923a
Adosz <adam.rakonczai97@gmail.com>
2022-08-03 09:51:42 +0200

    feat: add template

--- src/main/java/hu/radam/blackbelttest/controller/controller.java
+++ src/main/java/hu/radam/blackbelttest/controller/controller.java
@@ -1,5 +1,17 @@
 package hu.radam.blackbelttest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class controller {
-    
+
+    @GetMapping("/receipt/{rentalId}")
+    public String receipt(@PathVariable String rentalId){
+        return "receipt";
+    }
+
 }
